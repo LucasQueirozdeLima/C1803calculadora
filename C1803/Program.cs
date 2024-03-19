e9e61fba50e7a58e00d61eebd4ddf18110d2ac99
Tecnico <Tecnico@LAB3123>
2024-03-18 22:16:33 -0300

    Adicionar arquivos de projeto.

---
+++ C1803/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1803
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculadora calculo1 = new calculadora();
+            calculo1.calcular();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C1803/calculadora.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C1803
+{
+    class calculadora
+    {
+        public double n1;
+        public double n2;
+        public double total;
+        public int operador;
+        public string verificador;
+
+        public void calcular()
+        {
+            //entrada de valores
+            while (verificador != "N")
+            {
+
+                Console.WriteLine("\nDigite um valor: ");
+                this.n1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nDigite a operação:\n1.+\n2.-\n3.x\n4./\n ");
+                this.operador = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nDigite um valor: ");
+                this.n2 = double.Parse(Console.ReadLine());
+
+                //calculos
+
+                if (this.operador == 1)
+                {
+                    total = n1 + n2;
+                    Console.WriteLine("\n{0} + {1} = {2}", n1, n2, total);
+                }
+                if (this.operador == 2)
+                {
+                    total = n1 - n2;
+                    Console.WriteLine("\n{0} - {1} = {2}", n1, n2, total);
+                }
+                if (this.operador == 3)
+                {
+                    total = n1 * n2;
+                    Console.WriteLine("\n{0} x {1} = {2}", n1, n2, total);
+                }
+                if (this.operador == 4) if (this.n2 != 0)
+                    {
+                        total = n1 / n2;
+                        Console.WriteLine("\n{0} / {1} = {2}", n1, n2, total);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nERRO!!");
+                    }
+                Console.WriteLine("\nVocê deseja fazer outro calculo? S/N");
+                verificador = Console.ReadLine();
+            }
+
+            Console.WriteLine("Encerrando...");
+
+        }
+    }
+}
